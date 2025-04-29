3e93fdb6e413cf48b29c0f35689cb6fc4cb5a926
wald <manuwald16@gmail.com>
2025-04-29 12:36:14 -0300

    fix: remove audioRef prop from Visualizers

--- components/VisualizerWrapper.tsx
+++ components/VisualizerWrapper.tsx
@@ -20,10 +20,11 @@ const VisualizerWrapper: React.FC = () => {
 
     return (
         <div>
-            {visualizerIndex === 1 && <Visualizer1 audioRef={audioRef} />}
-            {visualizerIndex === 2 && <Visualizer2 audioRef={audioRef} />}
+            {visualizerIndex === 1 && <Visualizer1 />}
+            {visualizerIndex === 2 && <Visualizer2 />}
             {visualizerIndex === 3 && <Visualizer3 />}
             {visualizerIndex === 4 && <Visualizer4 />}
+
 
 
         </div>
