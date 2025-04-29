215e2cff950372b22d779a78cbdb19a81b57b0cd
wald <manuwald16@gmail.com>
2025-04-29 19:05:02 -0300

    updated uploadbutton, new music logo

--- components/UploadButton.tsx
+++ components/UploadButton.tsx
@@ -27,18 +27,21 @@ const UploadButton = () => {
       <button onClick={handleUploadClick} className="button">
         <svg
           className="w-6 h-6"
-          stroke="currentColor"
-          strokeWidth="1.5"
-          viewBox="0 0 24 24"
           fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          viewBox="0 0 24 24"
           xmlns="http://www.w3.org/2000/svg"
         >
           <path
-            d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
-            strokeLinejoin="round"
             strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M9 18V5l12-2v13"
           />
+          <circle cx="6" cy="18" r="3" stroke="currentColor" strokeWidth="1.5" />
+          <circle cx="18" cy="16" r="3" stroke="currentColor" strokeWidth="1.5" />
         </svg>
+
         <span className="text">Upload</span>
       </button>
       <input
